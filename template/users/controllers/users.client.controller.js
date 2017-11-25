c139eb5bf1f5ebed17fb3c29773befdf999ec77c
yuxian <andyretajeff@gmail.com>
2017-11-26 00:45:42 +0800

    grunt modified2

--- template/users/controllers/users.client.controller.js
+++ template/users/controllers/users.client.controller.js
@@ -1,20 +1,20 @@
 // Create the 'users' controller
 angular.module('users').controller('UsersController', 
-	['$scope', 'Authentication', '$location','$routeParams','Users','$http',
-	function($scope, Authentication, $location, $routeParams, Users, $http) {
+	['$scope', 'Authentication', '$location','$routeParams','$http',
+	function($scope, Authentication, $location, $routeParams, $http) {
 		// Expose the authentication service
 		$scope.authentication = Authentication;
 		$scope.signin = function(){
-			var users = new Users({
+			var users ={
                 username: this.username,
                 password: this.password
-            });
-			users.$save(function(response) {
-                window.location.reload("/_admin");
-                $location.path('/_admin');
-            }, function(errorResponse) {
-                $scope.error = errorResponse.data.message;
-            });
+            };
+            $http.post('api/_admin/signin',users).then(function (success){
+				window.location.reload("/_admin");
+				$location.path('/_admin');
+			},function (error){
+				$scope.error = errorResponse.data.message;
+			});
 		};
 		$scope.signout = function(){
             var users = {
