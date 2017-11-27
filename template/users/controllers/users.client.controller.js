2c36d0525981d5a7ca511a1a45fcc9f740ba4f65
yuxian <andyretajeff@gmail.com>
2017-11-27 20:44:32 +0800

    angular name scope change

--- template/users/controllers/users.client.controller.js
+++ template/users/controllers/users.client.controller.js
@@ -66,32 +66,34 @@ angular.module('users').controller('UsersController',
 
 		// };
 
-		// photos upload
-		$scope.submit = function(){ //function to call on form submit
-			if ($scope.upload_form.file.$valid && $scope.file) { //check if from is valid
-				$scope.upload($scope.file); //call upload function
-			}
-		};
 		
-		$scope.upload = function (file) {
-			Upload.upload({
-				url: '/api/_admin/upload', //webAPI exposed to upload the file
-				data:{file:file} //pass file as data, should be user ng-model
-			}).then(function (resp) { //upload function returns a promise
-				if(resp.data.error_code === 0){ //validate success
-					$window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
-				} else {
-					$window.alert('an error occured');
+		// photos upload
+		$scope.uploadphotos = {
+			submit : function(){ //function to call on form submit
+				if ($scope.upload_form.file.$valid && $scope.uploadphotos.file) { //check if from is valid
+					$scope.uploadphotos.upload($scope.uploadphotos.file); //call upload function
 				}
-			}, function (resp) { //catch error
-				console.log('Error status: ' + resp.status);
-				$window.alert('Error status: ' + resp.status);
-			}, function (evt) { 
-				console.log(evt);
-				var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-				console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-				$scope.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
-			});
+			},
+			upload : function (file) {
+				Upload.upload({
+					url: '/api/_admin/upload', //webAPI exposed to upload the file
+					data:{file:file} //pass file as data, should be user ng-model
+				}).then(function (resp) { //upload function returns a promise
+					if(resp.data.error_code === 0){ //validate success
+						$window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+					} else {
+						$window.alert('an error occured');
+					}
+				}, function (resp) { //catch error
+					console.log('Error status: ' + resp.status);
+					$window.alert('Error status: ' + resp.status);
+				}, function (evt) { 
+					console.log(evt);
+					var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+					console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+					$scope.uploadphotos.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
+				});
+			}
 		};
 		// /photos upload
 	}
