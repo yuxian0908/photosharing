3dc51594e0955ba0d4012daf8492f51e91e5273a
yuxian <andyretajeff@gmail.com>
2017-12-08 15:50:12 +0800

    chat demo test ok

--- app/modules/socketApi.js
+++ app/modules/socketApi.js
@@ -11,8 +11,16 @@ io.on('connection', function(socket){
     });
 });
 
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+});
+
 socketApi.sendNotification = function() {
     io.sockets.emit('hello', {msg: 'Hello World!'});
 };
+
+
 
 module.exports = socketApi;
