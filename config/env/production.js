e71ba8a4a27fa9bcb3bdae5f8e547f4b9dc7745c
yuxian <andyretajeff@gmail.com>
2017-12-17 15:35:45 +0800

    production config revised

--- config/env/production.js
+++ config/env/production.js
@@ -3,4 +3,12 @@ module.exports = {
 	db: 'mongodb://yuxian:andyreta551209@ds117336.mlab.com:17336/node6-test',
 	redis: 'redis://redis-14522.c16.us-east-1-3.ec2.cloud.redislabs.com:14522',
 	sessionSecret: 'productionSessionSecret',
+	cloudStorage: {
+		photofolder:'43341632199',
+		state: 'MyYuxianWebsite',
+		clientID: 'mze26rnupvitiyo9g9j2xamrx78e8gwo',
+		clientSecret: 'NofcjyrNTIDMGL4WUaGO5nRGeen38m3H',
+		redirect_uri: 'http://localhost:3000/cloud/redirect',
+		redirect_uri2:'http://localhost:3000/cloud/redirect2',
+	},
 };
