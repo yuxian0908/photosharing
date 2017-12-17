431d2db30774ae4b639d31a12b438b8c0bfbcef0
yuxian <andyretajeff@gmail.com>
2017-12-17 21:44:48 +0800

    cross app

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 
 app.use(function(req, res, next) { //allow cross origin requests
   res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-  res.header("Access-Control-Allow-Origin", "http://localhost");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
